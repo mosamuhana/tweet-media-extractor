0c71a2a80c5aa0c4136f3797e7c15479a312c9fd
Mosa Muhana <mosamuhana@gmail.com>
2022-06-06 08:41:28 +0300

    Update

--- src/http.ts
+++ src/http.ts
@@ -30,6 +30,7 @@ async function getRandomGuestID(): Promise<string> {
 }
 
 async function requestTweetMeta(tweetId: string, guestId: string) {
+	//const url = `https://api.twitter.com/2/timeline/conversation/${tweetId}.json?tweet_mode=extended`;
 	const url = `https://twitter.com/i/api/2/timeline/conversation/${tweetId}.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_ext_alt_text=true&include_quote_count=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweet=true&count=20&include_ext_has_birdwatch_notes=false&ext=mediaStats%2ChighlightedLabel`;
 	return await axios.get(url, { headers: { ...DEFAULT_HEADERS, 'x-guest-token': guestId } });
 }
--- src/tweet.ts
+++ src/tweet.ts
@@ -1,7 +1,6 @@
-import { getTweetData } from './http';
+import { Tweet, TweetMedia, VideoMedia, PhotoMedia } from './types';
 import { isValidTweetUrl, getTweetId } from './utils';
-import { Tweet } from './types';
-import { getTweetInfo, getTweetMediaList } from './parse-tweet';
+import { getTweetData } from './http';
 
 export async function getTweet(tweetUrl: string): Promise<Tweet> {
 	if (!isValidTweetUrl(tweetUrl)) {
@@ -17,4 +16,114 @@ export async function getTweet(tweetUrl: string): Promise<Tweet> {
 	}
 
 	return tweet;
+}
+
+function getTweetInfo(tweetData: any): Tweet {
+	const text = getTweetText(tweetData);
+
+	const {
+		id_str: id,
+		created_at,
+		favorite_count,
+		retweet_count,
+		reply_count,
+		quote_count,
+		lang,
+	} = tweetData;
+
+	const tweet: Tweet = {
+		id,
+		createdAt: new Date(created_at),
+		text,
+		favoriteCount: favorite_count,
+		retweetCount: retweet_count,
+		replyCount: reply_count,
+		quoteCount: quote_count,
+		lang,
+	};
+
+	return tweet;
+}
+
+function getTweetMediaList(tweetId: string, data: any): TweetMedia[] | undefined {
+	const ids = [ tweetId, ...getTimelineIds(data) ];
+	const tweets = ids.map(id => data.globalObjects.tweets[id]);
+	const mediaList = [];
+	for (const tweet of tweets) {
+		const media = getTweetMedia(tweet);
+		if (media) {
+			mediaList.push(...media);
+		}
+	}
+	return mediaList.length ? mediaList : undefined;
+}
+
+function getTweetMedia(tweetData: any): TweetMedia[] | undefined {
+	const list: TweetMedia[] = [];
+
+	if (tweetData.hasOwnProperty('extended_entities')) {
+		const mediaArray = tweetData.extended_entities.media as any[];
+		for (const media of mediaArray) {
+			if (media.type === 'video') {
+				const videos = getVideoMedia(media);
+				if (videos.length) {
+					list.push(...videos);
+				}
+			} else if (media.type === 'photo') {
+				list.push(getPhotoMedia(media));
+			}
+		}
+	}
+
+	return list.length ? list : undefined;
+}
+
+function getTimelineIds(data: any): string[] {
+	try {
+		// data.timeline.instructions[0].addEntries.entries[1].content.timelineModule.items
+		const instructions = data.timeline.instructions as any[];
+		const instruction = instructions.find(x => x.hasOwnProperty('addEntries'))
+		const entries = instruction.addEntries.entries as any[];
+		const entry = entries.find(x => x.hasOwnProperty('content') && x.content.hasOwnProperty('timelineModule'));
+		const items = entry.content.timelineModule.items as any[];
+		return items.map(x => x.item.content.tweet.id as string);
+	} catch (ex) {}
+	return [];
+}
+
+function getPhotoMedia(media: any): PhotoMedia {
+	return {
+		type: 'photo',
+		...getMediaSize(media),
+		url: media.media_url_https,
+		contentType: 'image/jpg'
+	};
+}
+
+function getVideoMedia(media: any): VideoMedia[] {
+	const variants = (media.video_info.variants as any[])
+		.filter(x => x.hasOwnProperty('bitrate'))
+		.sort((a: any, b: any) => b.bitrate - a.bitrate);
+
+	const size = getMediaSize(media);
+	const duration = media.video_info.duration_millis;
+	return variants.map(({ bitrate, url, content_type: contentType }) => ({
+		type: 'video',
+		bitrate,
+		url,
+		contentType,
+		duration,
+		...size,
+	} as VideoMedia));
+}
+
+function getTweetText(tweetData: any) {
+	const text: string = tweetData.text || tweetData.full_text || '';
+	const range = tweetData.display_text_range;
+	return range ? text.substring(range[0], range[1]) : text;
+}
+
+function getMediaSize(media: any) {
+	const { width, height } = media.original_info;
+	return { width, height };
 }
